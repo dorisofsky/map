04bc049ffe4740c9e09df6073a9a908e770c5573
Elsie Hsieh <8chingen8@gmail.com>
2015-07-31 16:12:42 +0800

    ff

--- templates/static/js/graphs.js
+++ templates/static/js/graphs.js
@@ -1,6 +1,6 @@
 queue()
     .defer(d3.json, "/donorschoose/projects")
-    .defer(d3.json, "./static/geojson/us-states.json")
+    .defer(d3.json, "http://ElsieHsieh.github.io/map/templates/static/geojson/us-states.json")
     .await(makeGraphs);
 
 function makeGraphs(error, projectsJson, statesJson) {
