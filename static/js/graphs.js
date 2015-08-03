913c9327c78fc11c523552754d0f0ae01ce69b34
Elsie Hsieh <8chingen8@gmail.com>
2015-08-03 14:59:48 +0800

    templates刪除

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,6 +1,6 @@
 queue()
     .defer(d3.csv, "http://ElsieHsieh.github.io/map/opendata_projects.csv", )
-    .defer(d3.json, "http://ElsieHsieh.github.io/map/templates/static/geojson/us-states.json")
+    .defer(d3.json, "http://ElsieHsieh.github.io/map/static/geojson/us-states.json")
     .await(makeGraphs);
 
 function makeGraphs(error, data, statesJson) {
